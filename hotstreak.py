0853824117b4c2e8fc0a6a5c34a56fc43eb6a603
gcallah <gcallah@mac.com>
2017-07-24 00:36:48 -0400

    HTML rebuild.

--- hotstreak.py
+++ hotstreak.py
@@ -7,17 +7,26 @@ Demonstrating that statistically undetectable hot streaks can exist.
 import random
 
 SHOTS = 50
+in_streak = False
+hot_streaks = 0
+hot_total = 0
 
 print("Shooting with hot streaks:")
 for shot in range(1, SHOTS):
     hot = (random.random() < .5)
     if hot:
+        hot_total += 1
+        if not in_streak:
+            in_streak = True
+            hot_streaks += 1
         make = (random.random() < .66)
     else:
+        in_streak = False
         make = (random.random() < .33)
     mark = 'X' if make else 'O'
     print(mark, end='')
 print("")
+print("Average hot streak length = " + str(hot_total / hot_streaks))
 
 print("Shooting without hot streaks:")
 for shot in range(1, SHOTS):
